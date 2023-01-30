57a13c0b6b9d051610552093ebbd3cb9c3a429eb
K4n1shk <k4n1shk1501@gmail.com>
2023-01-30 11:21:30 +0530

    Updates

---
+++ precache-manifest.a18b34dd872ac1a0d3a4a59c73a87e8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ee3279510036314f53c",
+    "url": "/video-streaming-app/static/css/main.b248875c.chunk.css"
+  },
+  {
+    "revision": "8ee3279510036314f53c",
+    "url": "/video-streaming-app/static/js/main.8ee32795.chunk.js"
+  },
+  {
+    "revision": "2497d9760f7eead18c44",
+    "url": "/video-streaming-app/static/js/1.2497d976.chunk.js"
+  },
+  {
+    "revision": "2622413b48519de1716b",
+    "url": "/video-streaming-app/static/js/runtime~main.2622413b.js"
+  },
+  {
+    "revision": "7166c55e611e1e839940567560c0b1be",
+    "url": "/video-streaming-app/index.html"
+  }
+];
